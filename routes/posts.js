54102b534b94e36c25fc04fefca9f025831bf83c
savioshaju <savioshajum@gmail.com>
2025-06-09 13:15:08 +0530

    first commit

--- routes/posts.js
+++ routes/posts.js
@@ -10,7 +10,15 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
   const db = await connectDB();
-  const result = await db.collection('posts').insertOne(req.body);
+  const { name, username, content,title, createdAt } = req.body;
+  const newPost = {
+    name,
+    username,
+    content,
+    title,
+    createdAt
+  }
+  const result = await db.collection('posts').insertOne(newPost);
   res.status(201).json({ insertedId: result.insertedId });
 });
 
