3ca6d130018109965d2170a939cbdfec39fac849
savioshaju <savioshajum@gmail.com>
2025-06-15 22:40:19 +0530

    New Blog edit

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
-const cors = require('cors'); 
+const cors = require('cors');
 const usersRouter = require('./routes/users');
 const postsRouter = require('./routes/posts');
 const summarizeRouter = require('./routes/summarize');
 
 const app = express();
 
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRouter);
 app.use('/api/posts', postsRouter);
 app.use('/api/summarize', summarizeRouter);
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
